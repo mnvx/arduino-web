02171a11d059ff2388ec45cab3ef071e974b35c8
mnv <mnvx@yandex.ru>
2014-01-08 02:09:12 +0400

    Output values into table

--- public/javascripts/getdata.js
+++ public/javascripts/getdata.js
@@ -1,1 +1,17 @@
-d3.selectAll("#data").text('test');
+var tr = d3.select("#data")
+  .append('table')
+    .selectAll('tr');
+
+tr.data([''])
+  .enter()
+  .append('tr')
+    .append('td')
+    .text('Value');
+
+d3.json("get-data", function(data) {
+  tr.data(data)
+    .enter()
+    .append('tr')
+      .append('td')
+      .text(function (d) { return d.value; });
+});
