4139fd5077c70b721b6b5c8f8599ca9d1330e3f1
mnv <mnvx@yandex.ru>
2014-01-08 02:25:31 +0400

    Added graphic instead of table

--- public/javascripts/getdata.js
+++ public/javascripts/getdata.js
@@ -1,17 +1,60 @@
-var tr = d3.select("#data")
-  .append('table')
-    .selectAll('tr');
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-tr.data([''])
-  .enter()
-  .append('tr')
-    .append('td')
-    .text('Value');
+//var parseDate = d3.time.format("%d-%b-%y").parse;
 
-d3.json("get-data", function(data) {
-  tr.data(data)
-    .enter()
-    .append('tr')
-      .append('td')
-      .text(function (d) { return d.value; });
+var x = d3.time.scale()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var line = d3.svg.line()
+    .x(function(d) { return x(d.time); })
+    .y(function(d) { return y(d.value); });
+
+var svg = d3.select("#data").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json("get-data", function(error, data) {
+  data.forEach(function(d) {
+    //d.time = parseDate(d.time);
+    d.time = Date.parse(d.time);
+    d.value = +d.value;
+  });
+
+  x.domain(d3.extent(data, function(d) { return d.time; }));
+  y.domain(d3.extent(data, function(d) { return d.value; }));
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Value");
+
+  svg.append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("d", line);
 });
